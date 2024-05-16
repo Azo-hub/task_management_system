9dba72e8426cac3730aa1e96bf8200c88ef63a4b
Azeez Ridwan <readone.cybernet@gmail.com>
2024-05-16 23:43:27 +0100

    created task entity

--- task_management_system/src/main/java/com/task_management_system/domain/Task.java
+++ task_management_system/src/main/java/com/task_management_system/domain/Task.java
@@ -1,4 +1,30 @@
 package com.task_management_system.domain;
 
+import com.task_management_system.utilities.SchemaConstants;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity(name= SchemaConstants.ENTITY_TASK)
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private long id;
+
+    @Column(columnDefinition = "text")
+    private String title;
+    @Column(columnDefinition = "text")
+    private String description;
+
+    private String priority;
+    private LocalDateTime dueDate;
+    private boolean completed = false;
+    private String createdAt;
+    private String updatedAt;
+    private LocalDateTime completedAt;
+
 }
---
+++ task_management_system/src/main/java/com/task_management_system/utilities/SchemaConstants.java
@@ -0,0 +1,5 @@
+package com.task_management_system.utilities;
+
+public class SchemaConstants {
+    public final static String ENTITY_TASK = "task";
+}
